3f1f58bab2bf588fd6e7ed1be20be5fcc2d95861
nersirion <nersirion@gmail.com>
2020-01-31 13:04:28 +0300

    Отредактирован class Features_Generator
    1. В функции features_processing_from_purchases подсчет обработанных клиентов изменен с count  на enumerate.
    Исправлены ошибки создания признаков (обращение шло не к тому словарю). Внесено открытие df_products внутрь функции.
    
    2.В функции create_features добавлены параметры(stop_number, skip_number)  для чтения файлы purchases по частям.
    Добавлено условие, что полученные датафреймы только сохраняются при параметре low_memory.
    Добавлен параметр, регулирующий нужна ли модель для корректировки возраста.
    Добавлены несколько принтов, для отоброжения, где протекает процесс.

--- Features_Generator.py
+++ Features_Generator.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+import time
+from time import gmtime, strftime
+from IPython.core.display import clear_output
+from collections import Counter
+
 class Features_Generator(object):
     
     '''Класс для преобразования имеющихся данных в два датасета признаков.
@@ -5,6 +13,7 @@ class Features_Generator(object):
     
     
     def __init__(self, path):
+        self.path = path
         self.products_path = path+'products.csv'
         self.purchases_path = path+'purchases.csv'
         self.clients_path = path+'clients.csv'
@@ -39,7 +48,8 @@ class Features_Generator(object):
    
 
     def features_processing_from_purchases(self, purchases):
-        
+        df_products=pd.read_csv(self.products_path).fillna('None')
+        df_products.loc[df_products['netto']=='None', 'netto']=0
         '''Это функция извлечения признаков из данных о покупках. Данные групируются вокруг клиента'''
         
         all_client_n = purchases['client_id'].nunique()
@@ -59,13 +69,13 @@ class Features_Generator(object):
         points_list = ['regular_points_received', 'express_points_received',\
                        'regular_points_spent', 'express_points_spent']
         data = []
-        count =0
-        for i, client in (purchases.groupby('client_id', sort=False)):
-            count+=1
-            if count%5000==0:
+        
+        for i, (id_c, client) in enumerate(purchases.groupby('client_id', sort=False)):
+            
+            if (i+1)%5000==0:
                 clear_output()
                 print(f'Начало обработки клиентов {start_time_for_print}')
-                print(f'Обработано {count} клиентов из {all_client_n}...')
+                print(f'Обработано {i+1} клиентов из {all_client_n}...')
                 print(f'C начало прошло {int(time.time()-start_time)} секунд')
                 
             features = features_product.copy()
@@ -108,18 +118,18 @@ class Features_Generator(object):
             #Features from points
             points_dict = dict(zip(client['transaction_id'].values, client[points_list].values))
             for point in points_dict.values():
-                features_product['regular_points_received']+=point[0]
-                features_product['express_points_received']+=point[1]
-                features_product['regular_points_spent']+=point[2]
-                features_product['express_points_spent']+=point[3]
+                features['regular_points_received']+=point[0]
+                features['express_points_received']+=point[1]
+                features['regular_points_spent']+=point[2]
+                features['express_points_spent']+=point[3]
             
                 
                 
             #Average features
-            features_product['avg_regular_points_received']=features_product['regular_points_received']/n_trans 
-            features_product['avg_express_points_received']=features_product['express_points_received']/n_trans  
-            features_product['avg_regular_points_spent']=features_product['regular_points_spent']/n_trans 
-            features_product['avg_express_points_spent']=features_product['express_points_spent']/n_trans  
+            features['avg_regular_points_received']=features['regular_points_received']/n_trans 
+            features['avg_express_points_received']=features['express_points_received']/n_trans  
+            features['avg_regular_points_spent']=features['regular_points_spent']/n_trans 
+            features['avg_express_points_spent']=features['express_points_spent']/n_trans  
             features['avg_sum_from_red'] = features['trn_sum_from_red']/n_trans  
             features['avg_price_product'] = features['sum']/n_trans
             features['avg_delay_beetwen_transc'] = features['transaction_max_delay']/n_trans   
@@ -130,12 +140,12 @@ class Features_Generator(object):
             features['avg_alhocol'] = features['alhocol']/n_trans
             
             data.append(features)
-        features_set = pd.DataFrame(data)    
+           
         clear_output()
         print(f'Начало обработки клиентов {start_time_for_print}')
         print(f'Обработка данных клиентов завершена за {int(time.time()-start_time)} секунд')
         
-        return features_set
+        return data
     
     def features_processing_from_clients_df(self, df_clients):
         df_clients['first_issue_unixtime'] = pd.to_datetime(df_clients['first_issue_date']).astype(np.int64)/10**9
@@ -155,6 +165,7 @@ class Features_Generator(object):
     def predict_broke_age(self, features_set):
         '''Это функция тренирует модель на предсказание возраста и делает предсказание ошибочного возраста.
         Под ошибочным понимается тот, который выбивается из интервала 14-95 лет'''
+        
         print('Запущена модель для корректировки неправильного возраста клиентов')
         broke_age_index = features_set[(features_set['age']<14) | (features_set['age']>95)].index
         
@@ -167,7 +178,7 @@ class Features_Generator(object):
             'objective': 'regression',
             'metric': 'rmse',
             'verbose': -1}
-        train_set = lgb.Dataset(X.iloc[:-5000], y[:-5000])
+        train_set = lgb.Dataset(X.iloc[:30000], y[:30000])
         val_set = lgb.Dataset(X.iloc[-5000:], y[-5000:])
         model = lgb.train(params=params,
                              train_set = train_set,
@@ -184,15 +195,19 @@ class Features_Generator(object):
         return features_set
     
     
-    def create_features(self, low_memory=True, save_files=None):
+    def create_features(self, low_memory=True, broke_age=True, save_files=None, skip_number=0, stop_number=0):
         
         '''Финальная функция, которая запускает все остальные. Подгружает датасеты и перерабатывает их в
         train and test DataFrames. Low_memory предназначена для ПК с малым количеством оперативной памяти.
-        Информация из датасета по покупкам подгружается постепенно.'''
+        Информация из датасета по покупкам подгружается постепенно.
+        broke_age - включает простую модель, которая предсказывает возраст клиента и исправляет явно ошибочный возраст.
+        skip_number - число chunk , которое нужно пропустить. Применяется если часть файла уже обработана.
+        stop_number - чило chunks на котором остановиться подрузка файла.
+        Это нужно, чтобы обработать файл в несколько подходов, для малой оперативной памяти.
+        '''
         
         start_time = time.time()
-        df_products=pd.read_csv(self.products_path).fillna('None')
-        df_products.loc[df_products['netto']=='None', 'netto']=0
+        
         df_clients = pd.read_csv(self.clients_path) 
         train = pd.read_csv(self.train_path) 
         test = pd.read_csv(self.test_path) 
@@ -200,24 +215,48 @@ class Features_Generator(object):
         if low_memory:
             purchases_chunks = pd.read_csv(self.purchases_path, chunksize=10**6)
             features_set = pd.DataFrame()
-            for chunk in purchases_chunks:
-                features_set = features_set.append(self.features_processing_from_purchases(chunk))
-        else:
-            features_set = self.features_processing_from_purchases(pd.read_csv(self.purchases_path))
+            data = []
+            count = 0
             
-        features_set = pd.merge(features_set, df_features, how='inner')
-        features_set = self.predict_broke_age(features_set)
+            for chunk in purchases_chunks: 
+                count+=1
+                if count>skip_number:
+                    chunk.fillna(0, inplace=True)
+                    print(f'Обрабатывается {count} chunk из {46} chunks')
+                    data+=self.features_processing_from_purchases(chunk)
+                    if count==stop_number:
+                        break
+            print('Создается общий датафрейм...')
+            features_set = pd.DataFrame(data)
+        else:
+            print('Создается общий датафрейм...')
+            features_set = pd.DataFrame(self.features_processing_from_purchases(pd.read_csv(self.purchases_path))).fillna(0)
         
+        
+        features_set = pd.merge(features_set, df_features, how='inner')
+        if broke_age:
+            features_set = self.predict_broke_age(features_set)
+        print('Создаются раздельные датафреймы...')
         features_set_train = pd.merge(features_set, train, how='inner')
         features_set_test = pd.merge(features_set, test, how='inner') 
         
         if save_files:
-            features_set_train.to_csv(path+'features_set_train', index=False)
-            features_set_test.to_csv(path+'features_set_test', index=False)
-            print(f'Файлы успешно сохранены в директорию {path}')
+            
+            if stop_number!=0:
+                new_path = self.path+'feat_sets/'
+                print(f'Данные сохраняются в директорию {new_path}...')
+                features_set_train.to_csv(new_path+'features_set_train_to_'+str(stop_number)+'.csv', index=False)
+                features_set_test.to_csv(new_path+'features_set_test_to_'+str(stop_number)+'.csv', index=False)
+                print(f'Файлы успешно сохранены в директорию {new_path}')
+            else:
+                print(f'Данные сохраняются в директорию {self.path}...')
+                features_set_train.to_csv(self.path+'features_set_train.csv', index=False)
+                features_set_test.to_csv(self.path+'features_set_test.csv', index=False)
+                print(f'Файлы успешно сохранены в директорию {self.path}')
             
         print(f'Весь процесс обработки данных занял {int(time.time()-start_time)} секунд')
         print(f'Тренировочный датасет состоит из обработанных данных {features_set_train.shape[0]} клиентов и {features_set_train.shape[1]-2} сгенерированных признаков')
         print(f'Тестовый датасет состоит из обработанных данных {features_set_test.shape[0]} клиентов и {features_set_test.shape[1]} сгенерированных признаков')
-        return features_set_train, features_set_test
         
+        if low_memory==False:
+            return features_set_train, features_set_test
