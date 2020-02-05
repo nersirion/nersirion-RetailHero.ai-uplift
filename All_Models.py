d818c593d47847119ab25bc812f6d3e1462e0497
nersirion <nersirion@gmail.com>
2020-02-05 14:06:10 +0300

    Отредактирован и подправлен класс All_Models.

--- All_Models.py
+++ All_Models.py
@@ -2,18 +2,30 @@ import xgboost as xgb
 import lightgbm as lgb
 import catboost as cat
 import numpy as np
-from Models import Models_Fundaments
+from IPython.core.display import clear_output
+from Models_Fundaments import Models_Fundaments
+
 class All_Models(Models_Fundaments):
-    '''Основной класс, в котором тренируются модели. Основан на классе Models_Fundaments'''
+    '''Основной класс, в котором тренируются модели для uplitf моделирования. Основан на классе Models_Fundaments'''
     
-    def start_training_xgb(self):
-        X,y, X_val, y_val, y_tr = self.preproccesing()
-
-
+    def start_training_xgb(self, target):
+        
+        ''' Бинарный классификатор основанный на xgb. Делает предсказание на train, test или val в зависимости от выбранного режима.
+            Важный момент, что предсказание на train set делается для всех данных, за исключением тех, на которых модель обучалась, 
+            т.е. train set может служишь ориентиром в подсчете uplift score'''
+        
+        
+        '''Сначала модели генерируются для данных, где treatment_flg==0'''
+        X = self.train[self.train['treatment_flg']==0].drop(self.cols_to_drop, axis=1)
+        y = self.train[self.train['treatment_flg']==0][target].values
         xgb_models_control=[]
         predict_control=[]
+        predict_train_0 = []
+        pred_temp = []
 
         for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
+            print(f'{fold+1} fold в процессе...')
+            print('------------------------')
             dtrain = xgb.DMatrix(X.iloc[train_ids], y[train_ids])
             dval = xgb.DMatrix(X.iloc[val_ids], y[val_ids])
             model = xgb.train(params=self.xgb_params,
@@ -21,77 +33,172 @@ class All_Models(Models_Fundaments):
                                           evals=[(dtrain, 'train'), (dval, 'val')],
                                           verbose_eval=200
                                          )
-            predict_control.append(model.predict(xgb.DMatrix(X_val)))
-        xgb_models_control.append(model)
-
-        X,y, X_val, y_val, y_tr = self.preproccesing(i=1)
-
+            pred = model.predict(xgb.DMatrix(X.iloc[val_ids]))
+            predict_train_0 = np.concatenate([pred, predict_train_0]) 
+            pred_temp.append(model.predict(xgb.DMatrix(self.train[self.train['treatment_flg']==1].drop(self.cols_to_drop, axis=1))))                                    
+            if self.mode=='val':
+                predict_control.append(model.predict(xgb.DMatrix(self.val.drop(self.cols_to_drop, axis=1)))) 
+            elif self.mode == 'test':
+                predict_control.append(model.predict(xgb.DMatrix(self.test.drop(self.drop_columns_test, axis=1))))
+            
+            xgb_models_control.append(model)
+        predict_train_0 = np.concatenate([predict_train_0, np.asarray(pred_temp).mean(axis=0)])      
+        clear_output()
+        
+        '''Во второй части модели берут данные, где treatment_flg==1'''
+        X = self.train[self.train['treatment_flg']==1].drop(self.cols_to_drop, axis=1)
+        y = self.train[self.train['treatment_flg']==1][target].values
         xgb_models_treatment=[]
         predict_treatment=[]
-
+        predict_train_1 = []
+        pred_temp = []
+        
         for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
+            print(f'{fold+1} fold в процессе...')
+            print('------------------------')
             dtrain = xgb.DMatrix(X.iloc[train_ids], y[train_ids])
             dval = xgb.DMatrix(X.iloc[val_ids], y[val_ids])
             model = xgb.train(params=self.xgb_params,
                                           dtrain=dtrain,
                                           evals=[(dtrain, 'train'), (dval, 'val')],
-                                          verbose_eval=200
-
+                                          verbose_eval=200,
+                                          early_stopping_rounds = 100
                                          )
-            predict_treatment.append(model.predict(xgb.DMatrix(X_val)))               
-        xgb_models_treatment.append(model)
-
-        xgb_predict_uplift = np.asarray(predict_treatment) - np.asarray(predict_control)
-        xgb_score = self.uplift_score(xgb_predict_uplift,  y_tr, y_val)
-
-        return xgb_predict_uplift
-                                    
-    def start_training_lgb(self):                              
-        X,y, X_val, y_val, y_tr = self.preproccesing()
-
-
-        lgb_models_control=[]
-        predict_control=[]
-
-        for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
-            train_set = lgb.Dataset(X.iloc[train_ids], y[train_ids])
-            val_set = lgb.Dataset(X.iloc[val_ids], y[val_ids])
-            model = lgb.train(params=self.lgb_params,
-                                              train_set=train_set,
-                                              valid_sets=[train_set, val_set],
-                                              verbose_eval=200
-                                             )
-            predict_control.append(model.predict((X_val), num_iteration=model.best_iteration))
-        lgb_models_control.append(model)
-
-        X,y, X_val, y_val, y_tr = self.preproccesing(i=1)
-
-        lgb_models_treatment=[]
-        predict_treatment=[]
-
-        for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
-            train_set = lgb.Dataset(X.iloc[train_ids], y[train_ids])
-            val_set = lgb.Dataset(X.iloc[val_ids], y[val_ids])
-            model = lgb.train(params=self.lgb_params,
-                                              train_set=train_set,
-                                              valid_sets=[train_set, val_set],
-                                              verbose_eval=200
-
-                                             )
-            predict_treatment.append(model.predict((X_val), num_iteration=model.best_iteration))             
-        lgb_models_treatment.append(model)
-
-        lgb_predict_uplift = np.asarray(predict_treatment) - np.asarray(predict_control)
-        lgb_score = self.uplift_score(lgb_predict_uplift,  y_tr, y_val)
-        return lgb_predict_uplift  
+            pred = model.predict(xgb.DMatrix(X.iloc[val_ids]))
+            predict_train_1 = np.concatenate([pred, predict_train_1])
+            pred_temp.appen(model.predict(xgb.DMatrix(self.train[self.train['treatment_flg']==0].drop(self.cols_to_drop, axis=1))))                 
+            if self.mode=='val':
+                predict_treatment.append(model.predict(xgb.DMatrix(self.val.drop(self.cols_to_drop, axis=1)))) 
+            elif self.mode == 'test':
+                predict_treatment.append(model.predict(xgb.DMatrix(self.test.drop(self.drop_columns_test, axis=1))))
+            xgb_models_treatment.append(model)
+        predict_train_1 = np.concatenate([predict_train_1, np.asarray(pred_temp).mean(axis=0)])           
+        clear_output()
             
-    def start_training_cat(self):
-        X,y, X_val, y_val, y_tr = self.preproccesing()
+        if self.mode == 'val':
+            treatment = self.val['treatment_flg']
+            target = self.val['target']
+            xgb_predict_uplift = (np.asarray(predict_treatment) - np.asarray(predict_control)).mean(axis=0)
+            xgb_score = self.uplift_score(xgb_predict_uplift,  treatment, target)
+            print(f'uplift score---{xgb_score}')
+            
+            return predict_control, predict_treatment, predict_train_0, predict_train_1
+        
+        elif self.mode == 'test':
+            xgb_predict_uplift = (np.asarray(predict_treatment) - np.asarray(predict_control)).mean(axis=0)
+            
+            return predict_control, predict_treatment, predict_train_0, predict_train_1, xgb_predict_uplift
+        
+
+    def start_training_lgb(self, target):   
+
+        ''' Бинарный классификатор основанный на xgb. Делает предсказание на train, test или val в зависимости от выбранного режима.
+            Важный момент, что предсказание на train set делается для всех данных, за исключением тех, на которых модель обучалась, 
+            т.е. train set может служишь ориентиром в подсчете uplift score'''
+        
+        
+        '''Сначала модели генерируются для данных, где treatment_flg==0'''                           
+        X = self.train[self.train['treatment_flg']==0].drop(self.cols_to_drop, axis=1)
+        y = self.train[self.train['treatment_flg']==0][target].values
 
 
-        cat_models_control=[]
-        predict_control=[]
+        lgb_models_control = []
+        predict_control = []
+        predict_train_0 = []
+        pred_temp = []
+
         for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
+            print(f'{fold+1} fold в процессе...')
+            print('------------------------')
+            train_set = lgb.Dataset(X.iloc[train_ids], y[train_ids])
+            val_set = lgb.Dataset(X.iloc[val_ids], y[val_ids])
+            model = lgb.train(params=self.lgb_params,
+                                              train_set=train_set,
+                                              valid_sets=[train_set, val_set],
+                                              verbose_eval=200,
+                                              early_stopping_rounds = 100
+                                             )
+            pred = model.predict((X.iloc[val_ids]), num_iteration=model.best_iteration)
+            predict_train_0 = np.concatenate([pred, predict_train_0])
+            pred_temp.append(model.predict((self.train[self.train['treatment_flg']==1].drop(self.cols_to_drop, axis=1)), num_iteration=model.best_iteration))     
+            if self.mode=='val':
+                predict_control.append(model.predict(self.val.drop(self.cols_to_drop, axis=1), num_iteration=model.best_iteration)) 
+            elif self.mode == 'test':
+                predict_control.append(model.predict(self.test.drop(self.drop_columns_test, axis=1), num_iteration=model.best_iteration))
+            lgb_models_control.append(model)       
+        predict_train_0 = np.concatenate([predict_train_0, np.asarray(pred_temp).mean(axis=0)])  
+        clear_output()
+        
+        
+        '''Во второй части модели берут данные, где treatment_flg==1'''
+        X = self.train[self.train['treatment_flg']==1].drop(self.cols_to_drop, axis=1)
+        y = self.train[self.train['treatment_flg']==1][target].values
+
+        lgb_models_treatment = []
+        predict_treatment = []
+        predict_train_1 = []
+        pred_temp = []
+
+        for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
+            print(f'{fold+1} fold в процессе...')
+            print('------------------------')
+            train_set = lgb.Dataset(X.iloc[train_ids], y[train_ids])
+            val_set = lgb.Dataset(X.iloc[val_ids], y[val_ids])
+            model = lgb.train(params=self.lgb_params,
+                                              train_set=train_set,
+                                              valid_sets=[train_set, val_set],
+                                              verbose_eval=200,
+                                              early_stopping_rounds = 100
+                                             )
+            pred = model.predict((X.iloc[val_ids]), num_iteration=model.best_iteration)
+            predict_train_1 = np.concatenate([pred, predict_train_1])
+            pred_temp.append(model.predict((self.train[self.train['treatment_flg']==0].drop(self.cols_to_drop, axis=1)), num_iteration=model.best_iteration))     
+            if self.mode=='val':
+                predict_treatment.append(model.predict(self.val.drop(self.cols_to_drop, axis=1), num_iteration=model.best_iteration)) 
+            elif self.mode == 'test':
+                predict_treatment.append(model.predict(self.test.drop(self.drop_columns_test, axis=1), num_iteration=model.best_iteration))
+                  
+            lgb_models_treatment.append(model)
+        predict_train_1 = np.concatenate([predict_train_1, np.asarray(pred_temp).mean(axis=0)])  
+        clear_output()
+        
+        if self.mode == 'val':
+            treatment = self.val['treatment_flg']
+            target = self.val['target']
+            lgb_predict_uplift = (np.asarray(predict_treatment) - np.asarray(predict_control)).mean(axis=0)
+            lgb_score = self.uplift_score(lgb_predict_uplift,  treatment, target)
+            print(f'uplift score---{lgb_score}')
+            
+            return predict_control, predict_treatment, predict_train_0, predict_train_1
+        
+        elif self.mode == 'test':
+            lgb_predict_uplift = (np.asarray(predict_treatment) - np.asarray(predict_control)).mean(axis=0)
+            
+            return predict_control, predict_treatment, predict_train_0, predict_train_1, lgb_predict_uplift
+
+     
+            
+    def start_training_cat(self, target):
+        
+        ''' Бинарный классификатор основанный на xgb. Делает предсказание на train, test или val в зависимости от выбранного режима.
+            Важный момент, что предсказание на train set делается для всех данных, за исключением тех, на которых модель обучалась, 
+            т.е. train set может служишь ориентиром в подсчете uplift score'''
+        
+        
+        
+        '''Сначала модели генерируются для данных, где treatment_flg==0'''
+        X = self.train[self.train['treatment_flg']==0].drop(self.cols_to_drop, axis=1)
+        y = self.train[self.train['treatment_flg']==0][target].values
+
+
+        cat_models_control = []
+        predict_control = []
+        predict_train_0 = []
+        pred_temp = []
+        
+        for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
+            print(f'{fold+1} fold в процессе...')
+            print('------------------------')
             dtrain = cat.Pool(X.iloc[train_ids], y[train_ids])
             dval = cat.Pool(X.iloc[val_ids], y[val_ids])
             model = cat.train(params=self.cat_params,
@@ -100,43 +207,68 @@ class All_Models(Models_Fundaments):
                                   verbose=200,
                                   early_stopping_rounds=100
                                  )
-                                             
-            predict_control.append(model.predict(cat.Pool(X_val)))
-        cat_models_control.append(model)
+            pred = model.predict(cat.Pool(X.iloc[val_ids]))
+            predict_train_0 = np.concatenate([pred, predict_train_0])
+            pred_temp.append(model.predict(cat.Pool(self.train[self.train['treatment_flg']==1].drop(self.cols_to_drop, axis=1))))                 
+            if self.mode=='val':
+                predict_control.append(model.predict(cat.Pool(self.val.drop(self.cols_to_drop, axis=1)))) 
+            elif self.mode == 'test':
+                predict_control.append(model.predict(cat.Pool(self.test.drop(self.drop_columns_test, axis=1))))    
+                             
+            cat_models_control.append(model)
+        predict_train_0 = np.concatenate([predict_train_0, np.asarray(pred_temp).mean(axis=0)])        
+        clear_output()
 
-        X,y, X_val, y_val, y_tr = self.preproccesing(i=1)
+        '''Во второй части модели берут данные, где treatment_flg==1'''
+        X = self.train[self.train['treatment_flg']==1].drop(self.cols_to_drop, axis=1)
+        y = self.train[self.train['treatment_flg']==1][target].values
 
-        cat_models_treatment=[]
-        predict_treatment=[]
+        cat_models_treatment = []
+        predict_treatment = []
+        predict_train_1 = []
+        pred_temp = []
+        
         for fold, (train_ids, val_ids) in enumerate(self.folds.split(X,y)):
+            print(f'{fold+1} fold в процессе...')
+            print('------------------------')
             train_set = cat.Pool(X.iloc[train_ids], y[train_ids])
             val_set = cat.Pool(X.iloc[val_ids], y[val_ids])
             model = cat.train(params=self.cat_params,
                                               dtrain=train_set,
-                                              eval_set=dval,
+                                              eval_set=val_set,
                                               verbose=200,
                                               early_stopping_rounds=100
 
                                              )
-            predict_treatment.append(model.predict(cat.Pool(X_val)))             
-        cat_models_treatment.append(model)
+            pred = model.predict(cat.Pool(X.iloc[val_ids]))
+            predict_train_1 = np.concatenate([pred, predict_train_1])    
+            pred_temp.append(model.predict(cat.Pool(self.train[self.train['treatment_flg']==0].drop(self.cols_to_drop, axis=1))))        
+            if self.mode=='val':
+                predict_treatment.append(model.predict(cat.Pool(self.val.drop(self.cols_to_drop, axis=1)))) 
+            elif self.mode == 'test':
+                predict_treatment.append(model.predict(cat.Pool(self.test.drop(self.drop_columns_test, axis=1))))        
+                      
+            cat_models_treatment.append(model)
+       
+        predict_train_1 = np.concatenate([predict_train_1, np.asarray(pred_temp).mean(axis=0)])  
+        clear_output()
 
-        cat_predict_uplift = np.asarray(predict_treatment) - np.asarray(predict_control)
-        cat_score = self.uplift_score(cat_predict_uplift,  y_tr, y_val)
-        return cat_predict_uplift 
+        if self.mode == 'val':
+            treatment = self.val['treatment_flg']
+            target = self.val['target']
+            cat_predict_uplift = (np.asarray(predict_treatment) - np.asarray(predict_control)).mean(axis=0)
+            cat_score = self.uplift_score(cat_predict_uplift,  treatment, target)
+            print(f'uplift score---{cat_score}')
+            
+            return predict_control, predict_treatment, predict_train_0, predict_train_1
+        
+        elif self.mode == 'test':
+            cat_predict_uplift = (np.asarray(predict_treatment) - np.asarray(predict_control)).mean(axis=0)
+            
+            return predict_control, predict_treatment, predict_train_0, predict_train_1, cat_predict_uplift
+
+      
     
-    def uplift_score_calculation(self):
-        X,y, X_val, y_val, y_tr = self.preproccesing(i=1)
-        xgb_score = self.start_training_xgb()
-        lgb_score = self.start_training_lgb()
-        cat_score = self.start_training_cat()
-        
-        uplift_score = (xgb_score+lgb_score+cat_score)/3
-        uplift_score_2 = (xgb_score*0.2+lgb_score*0.5+cat_score*0.3)
-        score = self.uplift_score(uplift_score,  y_tr, y_val)
-        score_2 = self.uplift_score(uplift_score_2,  y_tr, y_val)
-        return score, score_2
-        
         
         
         
